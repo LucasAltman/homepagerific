25aad6acc95d4dd5bf651ea0626c4959a4aad39c
Lucas Altman <lucas.altman@gmail.com>
2014-03-18 21:53:55 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homepagerific::Application.config.secret_key_base = '6504a04c1f1add56a88d2f1fdb74812df0ad1f1ac7c454f428691514b806cb23bf844197769e0fd74c0f66b5e383a0a76e21943f0263dfb0c921225d9dce8c43'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Homepagerific::Application.config.session_store :cookie_store, key: '_homepagerific_session'
